8c4cbd9b937be1591404365c397562960c12d9b0
brettmorrisonA05ANNEX <brettsterm99@gmail.com>
2026-04-19 12:00:08 -0700

    further progress on spn re themeing

--- animations/test.py
+++ animations/test.py
@@ -34,10 +34,10 @@ class TestScene(Scene):
         pt_text = Text("plaintext", font_size=28, color=WHITE).shift(LEFT * 4)
         ct_text = Text("ciphertext", font_size=28, color=WHITE).shift(RIGHT * 4)
 
-        input_arrow = Arrow(pt_text.get_right(), s_box.get_left(), color=WHITE)
-        s_to_p_arrow = CurvedArrow(s_box.get_bottom(), p_box.get_bottom())
-        p_to_s_arrow = CurvedArrow(p_box.get_top(), s_box.get_top())
-        output_arrow = Arrow(p_box.get_right(), ct_text.get_left())
+        input_arrow = Arrow(pt_text.get_right(), s_box.get_left(), color=custom_colors.FOREGROUND_1)
+        s_to_p_arrow = CurvedArrow(s_box.get_bottom(), p_box.get_bottom(), color=custom_colors.FOREGROUND_1)
+        p_to_s_arrow = CurvedArrow(p_box.get_top(), s_box.get_top(), color=custom_colors.FOREGROUND_1)
+        output_arrow = Arrow(p_box.get_right(), ct_text.get_left(), color=custom_colors.FOREGROUND_1)
 
         self.play(FadeIn(s_box),
                   FadeIn(p_box))
@@ -54,8 +54,8 @@ class TestScene(Scene):
         self.wait(1)
 
         #----------- SECTION 3 -----------#
-        # SPN introduction
-        self.next_section(skip_animations=0)
+        # S-Box intro
+        self.next_section(skip_animations=1)
         #---------------------------------#
         self.play(FadeOut(pt_text),
                   FadeOut(ct_text),
@@ -65,7 +65,6 @@ class TestScene(Scene):
                   FadeOut(p_to_s_arrow),
                   FadeOut(output_arrow),
                   s_box.animate.move_to(ORIGIN))
-        self.wait(1)
 
         sub_table_rect = RoundedRectangle(corner_radius=0.25, height=1, width=2, color=custom_colors.RED, fill_opacity=0)
         separator = Line(sub_table_rect.get_top(), sub_table_rect.get_bottom(), color=custom_colors.RED)
@@ -75,8 +74,8 @@ class TestScene(Scene):
         output_byte_0 = MathTex(r"\left\{ b_0'\right\}").move_to(sub_table.get_center() + RIGHT * 0.5)
         output_byte_1 = MathTex(r"\left\{ b_1'\right\}").shift(RIGHT * 3)
 
-        input_arrow = Arrow(input_byte.get_right(), s_box.get_left())
-        output_arrow = Arrow(s_box.get_right(), output_byte_1.get_left())
+        input_arrow = Arrow(input_byte.get_right(), s_box.get_left(), color=custom_colors.FOREGROUND_1)
+        output_arrow = Arrow(s_box.get_right(), output_byte_1.get_left(), color=custom_colors.FOREGROUND_1)
 
         self.play(FadeIn(input_byte),
                   FadeIn(sub_table),
@@ -88,6 +87,59 @@ class TestScene(Scene):
         self.play(input_byte_copy.animate.move_to(sub_table.get_center() + LEFT * 0.5))
         self.play(GrowArrow(output_arrow),
                   output_byte_0.animate.move_to(s_box.get_center() + RIGHT * 3))
+
+        #----------- SECTION 4 -----------#
+        # S-Box properties
+        self.next_section(skip_animations=1)
+        #---------------------------------#
+        self.play(FadeOut(sub_table),
+                  FadeOut(input_byte_copy))
+        
+        original_sub_group = VGroup(input_byte, input_arrow, s_box, output_arrow, output_byte_0)
+        green_rect = RoundedRectangle(corner_radius=0.25, width=8, height=1.5, color=custom_colors.GREEN, fill_opacity=0).shift(UP * 1.5)
+
+        invalid_sub_group = original_sub_group.copy().shift(DOWN * 1.5)
+        red_rect = RoundedRectangle(corner_radius=0.25, width=8, height=3, color=custom_colors.RED, fill_opacity=0).shift(DOWN * 1.5)
+
+        # redefine group elements for non-one to one animation
+        invalid_sub_group[2][0].set_color(color=custom_colors.RED)
+        output_byte_1.move_to(invalid_sub_group[4].get_center() + DOWN)
+        output_arrow_1 = Arrow(invalid_sub_group[2].get_right(), output_byte_1.get_left(), color=custom_colors.FOREGROUND_1)
+        invalid_sub_group[3] = always_redraw(
+            lambda: Arrow(invalid_sub_group[2].get_right(), invalid_sub_group[4].get_left(), color=custom_colors.FOREGROUND_1))
+                                             
+        self.play(original_sub_group.animate.shift(UP * 1.5),
+                  FadeIn(green_rect))
+        self.play(FadeIn(invalid_sub_group),
+                  FadeIn(red_rect))
+        self.play(invalid_sub_group[4].animate.shift(UP),
+                  GrowArrow(output_arrow_1),
+                  FadeIn(output_byte_1))
+        
+        #----------- SECTION 5 -----------#
+        # P-Box properties
+        self.next_section(skip_animations=0)
+        #---------------------------------#
+        p_box.move_to(ORIGIN)
+
+        self.play(FadeOut(original_sub_group),
+                  FadeOut(green_rect),
+                  FadeOut(invalid_sub_group),
+                  FadeOut(red_rect),
+                  FadeOut(output_arrow_1),
+                  FadeOut(output_byte_1),
+                  FadeIn(p_box), run_time=1)
+        
+        input_byte = MathTable([["b_0", "b_1", "b_2", "b_3", "b_4", "b_5", "b_6", "b_7"]], 
+                               include_outer_lines=True,
+                               v_buff=1,
+                               h_buff=1).shift(UP * 1.5).scale(0.5)
+        output_byte = input_byte.copy().move_to(ORIGIN).shift(DOWN * 1.5)
+        
+        self.play(FadeIn(input_byte),
+                  FadeIn(output_byte))
+        
+        
 
 
 
